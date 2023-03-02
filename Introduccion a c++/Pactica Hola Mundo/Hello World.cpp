f651edd9f31f33d3a65aad8d4602876eaea31402
Aicama <snapfire70@gmail.com>
2023-03-02 12:35:13 -0500

    first commit

---
+++ Introduccion a c++/Pactica Hola Mundo/Hello World.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+// Jose Miguel Frade IPVCE MH7 Artemisa
+//================macros=====================
+// Maximo
+#define mx 10000000
+// Optimizacion
+#define fast     \
+    cin.tie(0);  \
+    cout.tie(0); \
+    ios_base::sync_with_stdio(0);
+// Constantes
+#define pi 3.141592
+// salto de linea
+#define sl "\n"
+// varibales
+#define ull unsigned long long
+#define ll long long
+#define ii pair<int, int>
+#define vi vector<int>
+#define vii vector<ii>
+#define qi queue<int>
+#define sei set<int>
+// Comandos vector
+#define PB push_back
+#define ins(a, b) insert(make_pair(a, b))
+// Comandos queue
+#define P push
+#define FR front()
+#define PP pop()
+// Comandos para set
+#define IN insert
+#define C count
+// Comandos para string
+#define str to_string
+// Comandos Pair
+#define F first
+#define S second
+//===========================================
+int main()
+{
+    fast
+    cout << "Hola Mundo";
+}
---
+++ Introduccion a c++/Practica de operaciones basicas matematicas/evaluando formulas de sucesion .cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Jose Miguel Frade IPVCE MH7 Artemisa
+//================macros=====================
+//Maximo
+#define mx 10000000
+//Optimizacion
+#define fast     \
+    cin.tie(0);  \
+    cout.tie(0); \
+    ios_base::sync_with_stdio(0);
+//Constantes
+#define pi 3.14159265359
+//salto de linea
+#define sl "\n"
+//varibales
+#define ull unsigned long long
+#define ll long long
+#define ii pair<int, int>
+#define vi vector<int>
+#define vii vector<ii>
+#define qi queue<int>
+#define sei set<int>
+//Comandos vector
+#define PB push_back
+#define ins(a,b) insert(make_pair(a,b))
+//Comandos queue
+#define P push
+#define FR front()
+#define PP pop()
+//Comandos para set
+#define IN insert
+#define C count
+//Comandos para string
+#define str to_string
+//Comandos Pair
+#define F first
+#define S second
+//===========================================
+int main(){
+    fast
+    double x;
+    cin >> x;
+    double y = (x+5)/(2*(x+1));
+    cout << fixed << setprecision(6) << (pow(y,2)+(x*(x-(2*y))))/(x*y);
+}
